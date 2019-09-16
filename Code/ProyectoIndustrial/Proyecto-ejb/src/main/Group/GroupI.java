46547e45d06338e64c5444bb151be2d948c13c33
angelRacancoj <the1999evilrg@gmail.com>
2019-09-15 19:55:47 -0600

    se han creado la tabla de grupos e historial, se ha cambiando en historial costo_total de VARCHAR a DOUBLE

---
+++ Code/ProyectoIndustrial/Proyecto-ejb/src/main/Group/GroupI.java
@@ -0,0 +1,55 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author angelrg
+ */
+
+@Entity
+@Table(
+        name = "GRUPO"
+)
+public class GroupI {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @Column(name = "id_grupo")
+    private Integer idGroup;
+    @Column(name = "informacion", insertable = true, updatable = true)
+    private String infomation;
+    @Column(name = "seccion", insertable = true, updatable = true)
+    private String section;
+
+    public GroupI() {
+    }
+
+    public Integer getIdGroup() {
+        return idGroup;
+    }
+
+    public void setIdGroup(Integer idGroup) {
+        this.idGroup = idGroup;
+    }
+
+    public String getInfomation() {
+        return infomation;
+    }
+
+    public void setInfomation(String infomation) {
+        this.infomation = infomation;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+ 
+    
+}
---
+++ Code/ProyectoIndustrial/Proyecto-ejb/src/main/Group/GroupUser.java
@@ -0,0 +1,64 @@
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(
+        name = "GRUPO_USUARIO"
+)
+
+public class GroupUser {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @Column(name = "id_grupo_usuario")
+    private Integer id_group_user;
+    @ManytoOne
+    private User user;
+    @ManytoOne
+    private GroupI group;
+    @Column(name = "fecha_ingreso", insertable = true, updatable = true)
+    @Temporal(TemporalType.DATE)
+    private Date admissionDate;
+
+    public GroupUser() {
+    }
+
+    public Integer getId_group_user() {
+        return id_group_user;
+    }
+
+    public void setId_group_user(Integer id_group_user) {
+        this.id_group_user = id_group_user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public GroupI getGroup() {
+        return group;
+    }
+
+    public void setGroup(GroupI group) {
+        this.group = group;
+    }
+
+    public Date getAdmissionDate() {
+        return admissionDate;
+    }
+
+    public void setAdmissionDate(Date admissionDate) {
+        this.admissionDate = admissionDate;
+    }    
+    
+}
