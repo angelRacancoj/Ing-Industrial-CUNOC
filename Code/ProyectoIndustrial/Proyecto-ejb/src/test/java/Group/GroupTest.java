c8fee2741a26be1998e3abd0bf871cc96df78816
angelRacancoj <the1999evilrg@gmail.com>
2019-09-20 10:50:55 -0600

    Se ha agregado las clases y metodos para las clases Group y UserGroup

--- Code/ProyectoIndustrial/Proyecto-ejb/src/test/java/Group/GroupTest.java
+++ Code/ProyectoIndustrial/Proyecto-ejb/src/test/java/Group/GroupTest.java
@@ -5,25 +5,54 @@ import org.testng.annotations.Test;
 
 /**
  * Proyecto-ejb
- * @author jose - 18.09.2019 
+ *
+ * @author jose - 18.09.2019
  * @Title: GroupTest
  * @Description: description
  *
  * Changes History
  */
 public class GroupTest {
-    
+
     @Test
     public void testSetAndGetIdGroup() throws Exception {
         // Arrange
         Group testGroup = new Group();
         int testVal = 100;
-        
+
         // Act
         testGroup.setIdGroup(testVal);
         int result = testGroup.getIdGroup();
-        
+
         // Assert
         Assert.assertEquals(result, testVal);
+    }
+
+    @Test
+    public void testSetAndGetInformation() throws Exception {
+        //Arrange
+        Group testGroup = new Group();
+        String testText = "Este es un texto de prueba";
+
+        //Act
+        testGroup.setInfomation(testText);
+        String result = testGroup.getInfomation();
+
+        //Assert
+        Assert.assertEquals(result, testText);
+    }
+
+    @Test
+    public void testSetAndGetSection() throws Exception {
+        //Arange
+        Group testGroup = new Group();
+        String testText = "A";
+
+        //Act
+        testGroup.setSection(testText);
+        String result = testGroup.getSection();
+
+        //Assert
+        Assert.assertEquals(result, testText);
     }
 }
---
+++ Code/ProyectoIndustrial/Proyecto-ejb/src/test/java/Group/GroupUserTest.java
@@ -0,0 +1,59 @@
+package Group;
+
+import User.User;
+import java.time.Instant;
+import java.util.Date;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author angelrg
+ */
+public class GroupUserTest {
+    
+    @Test
+    public void testSetAndGetIdGroupUser() throws Exception{
+        //Arrange
+        GroupUser testGroupUser = new GroupUser();
+        int testVal =123;
+        
+        //Act
+        testGroupUser.setidGroupUser(testVal);
+        int result = testGroupUser.getidGroupUser();
+        
+        //Assert
+        Assert.assertEquals(result, testVal);
+    }
+    
+    @Test
+    public void testSetAndGetUser() throws Exception {
+        //Arrange
+       GroupUser testGroupUser = new GroupUser();
+//       User user = new User
+    }
+    
+    @Test
+    public void testSetAndGetIdGroupGroup() throws Exception{
+        //Arrange
+        GroupUser testGroupUser = new GroupUser();
+        
+        //Act
+        
+        //Assert
+    }
+    
+    @Test
+    public void testSetAndGetIdGroupAdmissionDate() throws Exception{
+        //Arrange
+        GroupUser testGroupUser = new GroupUser();
+        Date testDate = Date.from(Instant.now());
+        
+        //Act
+        testGroupUser.setAdmissionDate(testDate);
+        Date result = testGroupUser.getAdmissionDate();
+        
+        //Assert
+        Assert.assertEquals(result, testDate);
+    }
+}
