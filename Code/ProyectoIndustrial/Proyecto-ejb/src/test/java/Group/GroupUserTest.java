2a6e87e1c4b3a2d50b2a281d460c69be45167fe3
angelRacancoj <the1999evilrg@gmail.com>
2019-09-20 19:22:14 -0600

    Merge with master

--- Code/ProyectoIndustrial/Proyecto-ejb/src/test/java/Group/GroupUserTest.java
+++ Code/ProyectoIndustrial/Proyecto-ejb/src/test/java/Group/GroupUserTest.java
@@ -11,48 +11,47 @@ import org.testng.annotations.Test;
  * @author angelrg
  */
 public class GroupUserTest {
-    
+
     @Test
-    public void testSetAndGetIdGroupUser() throws Exception{
+    public void testSetAndGetIdGroupUser() throws Exception {
         //Arrange
         GroupUser testGroupUser = new GroupUser();
-        int testVal =123;
-        
+        int testVal = 123;
+
         //Act
         testGroupUser.setidGroupUser(testVal);
         int result = testGroupUser.getidGroupUser();
-        
+
         //Assert
         Assert.assertEquals(result, testVal);
     }
-    
+
     @Test
     public void testSetAndGetUser() throws Exception {
         //Arrange
-       GroupUser testGroupUser = new GroupUser();
+        GroupUser testGroupUser = new GroupUser();
 //       User user = new User
     }
-    
+
     @Test
-    public void testSetAndGetIdGroupGroup() throws Exception{
+    public void testSetAndGetIdGroupGroup() throws Exception {
         //Arrange
         GroupUser testGroupUser = new GroupUser();
-        
+
         //Act
-        
         //Assert
     }
-    
+
     @Test
-    public void testSetAndGetIdGroupAdmissionDate() throws Exception{
+    public void testSetAndGetIdGroupAdmissionDate() throws Exception {
         //Arrange
         GroupUser testGroupUser = new GroupUser();
         Date testDate = Date.from(Instant.now());
-        
+
         //Act
         testGroupUser.setAdmissionDate(testDate);
         Date result = testGroupUser.getAdmissionDate();
-        
+
         //Assert
         Assert.assertEquals(result, testDate);
     }
