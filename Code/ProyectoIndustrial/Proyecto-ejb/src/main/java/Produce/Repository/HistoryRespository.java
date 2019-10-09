a32cc481d81a9f485a876cac0f4db0427651ff10
angelRacancoj <orlan_1397rg@hotmail.com>
2019-10-09 12:49:46 -0600

    se agrego el REPOSITORY class de HISTORY

--- Code/ProyectoIndustrial/Proyecto-ejb/src/main/java/Produce/Repository/HistoryRespository.java
+++ Code/ProyectoIndustrial/Proyecto-ejb/src/main/java/Produce/Repository/HistoryRespository.java
@@ -1,10 +1,15 @@
 package Produce.Repository;
 
+import Produce.History;
 import static config.Constants.PERSISTENCE_UNIT_NAME;
+import java.util.List;
+import java.util.Optional;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
 
 /**
  *
@@ -17,6 +22,16 @@ public class HistoryRespository {
     @PersistenceContext(name = PERSISTENCE_UNIT_NAME)
     private EntityManager entityManager;
 
-    
-    
+    public Optional<History> findById(int id) {
+        return Optional.of(entityManager.find(History.class, id));
+    }
+
+    public Optional<List<History>> getAll() {
+        CriteriaQuery criteriaQuery = entityManager.getCriteriaBuilder().createQuery();
+        criteriaQuery.select(criteriaQuery.from(History.class));
+
+        Query query = entityManager.createQuery(criteriaQuery);
+        return Optional.of(query.getResultList());
+    }
+
 }
