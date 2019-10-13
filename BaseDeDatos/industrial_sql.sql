e0d7dbab62930e7240494ab97836923c47b67cfc
danielgonzalezdagg <danielgonzalezdagg@gmail.com>
2019-10-13 01:20:34 -0600

    Arreglando Errores

--- BaseDeDatos/industrial_sql.sql
+++ BaseDeDatos/industrial_sql.sql
@@ -13,7 +13,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 DROP SCHEMA IF EXISTS `produccion_industrial` ;
 
 -- -----------------------------------------------------
--- Schema produccion_industrial
+-- Schema produccion_industrial  
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `produccion_industrial` ;
 USE `produccion_industrial` ;
