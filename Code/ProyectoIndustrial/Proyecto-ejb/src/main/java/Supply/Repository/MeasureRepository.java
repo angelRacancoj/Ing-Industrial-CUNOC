66b905d25574d77b15eaa4e6b0e66875942c1c4d
cryraf2095 <cryraf2095@gmail.com>
2019-10-14 18:54:34 -0600

    add MeasureRepository

---
+++ Code/ProyectoIndustrial/Proyecto-ejb/src/main/java/Supply/Repository/MeasureRepository.java
@@ -0,0 +1,29 @@
+
+package Supply.Repository;
+
+import Supply.Measure;
+import static config.Constants.PERSISTENCE_UNIT_NAME;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+public class MeasureRepository {
+    @PersistenceContext(name = PERSISTENCE_UNIT_NAME)
+    private EntityManager entityManager;
+
+    public MeasureRepository() {
+    }
+    
+    public List<Measure> getMeasures(){
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Measure> criteriaQuery = criteriaBuilder.createQuery(Measure.class);
+        Root<Measure> measure = criteriaQuery.from(Measure.class);
+        criteriaQuery.select(measure);
+        TypedQuery<Measure> query = entityManager.createQuery(criteriaQuery);
+        return query.getResultList();
+    }
+}
