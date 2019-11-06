6be6249a19081dbdcacc635a42567b2936fd5c20
angelRacancoj <orlan_1397rg@hotmail.com>
2019-11-06 15:40:34 -0600

    Se agregaron varios facades, pero necesitan logica q aun no existe dentro del programa

--- Code/ProyectoIndustrial/Proyecto-ejb/src/main/java/Inventory/Inventory.java
+++ Code/ProyectoIndustrial/Proyecto-ejb/src/main/java/Inventory/Inventory.java
@@ -1,10 +1,12 @@
 package Inventory;
 
+import Production.Production;
 import Supply.Supply;
 import Supply.repository.AvailabilityFilter;
 import Supply.repository.ExpirationDateFilter;
 import Supply.repository.SupplyRepository;
 import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 /**
@@ -14,13 +16,52 @@ import javax.ejb.Stateless;
 @Stateless
 public class Inventory implements InventoryLocal {
 
-    SupplyRepository supplyRepository;
-    
-    
+    private SupplyRepository supplyRepository;
+
+    @EJB
+    public void setSupplyRepository(SupplyRepository supplyRepository) {
+        this.supplyRepository = supplyRepository;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<Supply> getSupply(Integer codeSupply, String nameSupply, AvailabilityFilter availabilitySupply, ExpirationDateFilter expirationDateSupply) {
         return supplyRepository.getSupply(codeSupply, nameSupply, availabilitySupply, expirationDateSupply);
     }
 
-    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Supply> getExpiredSupplys() {
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Production> getBestProductsBaseOnAvailableMaterial() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Production> calculateProductOnMaxCost(Double maxCost) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Double> costByPruductionAndBatch(List<Production> selectedProduction) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
--- Code/ProyectoIndustrial/Proyecto-ejb/src/main/java/Inventory/InventoryLocal.java
+++ Code/ProyectoIndustrial/Proyecto-ejb/src/main/java/Inventory/InventoryLocal.java
@@ -5,9 +5,12 @@
  */
 package Inventory;
 
+import Production.Product;
+import Production.Production;
 import Supply.Supply;
 import Supply.repository.AvailabilityFilter;
 import Supply.repository.ExpirationDateFilter;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.Local;
 
@@ -18,5 +21,54 @@ import javax.ejb.Local;
 @Local
 public interface InventoryLocal {
 
+    /**
+     *
+     * Get all supplies in Data Base, is available to use filter for searching
+     * based in the parameters, to unable a filter send a NULL
+     *
+     * To get all supply send all parameters with NULL
+     *
+     * @param codeSupply
+     * @param nameSupply
+     * @param availabilitySupply
+     * @param expirationDateSupply
+     * @return
+     */
     public List<Supply> getSupply(Integer codeSupply, String nameSupply, AvailabilityFilter availabilitySupply, ExpirationDateFilter expirationDateSupply);
+
+    /**
+     * Return list of supply expired to the current date
+     *
+     * @return
+     */
+    public List<Supply> getExpiredSupplys();
+
+    /**
+     * Calculate the best Production to produce base on material available
+     *
+     * @return
+     */
+    public List<Production> getBestProductsBaseOnAvailableMaterial();
+
+    /**
+     * Base on available money calculate the best products to produce base on
+     * higher note of production by Product
+     *
+     * TODO object (Production,batch)
+     *
+     * @param maxCost
+     * @return
+     */
+    public List<Production> calculateProductOnMaxCost(Double maxCost);
+
+    /**
+     * Get the cost base on selected Production (selectedProductions) and
+     * Quantity of batches
+     *
+     * TODO object (Production,batch)
+     *
+     * @param selectedProduction
+     * @return
+     */
+    public List<Double> costByPruductionAndBatch(List<Production> selectedProduction);
 }
