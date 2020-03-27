8333f6ed46291131c3d42b8caab725e32c0b66e4
angelRacancoj <orlan_1397rg@hotmail.com>
2020-03-27 08:04:16 -0600

    Se puede indicar la cantidad de elementos en supply calculator

--- Code/ProyectoIndustrial/Proyecto-war/src/main/java/gt/udu/usac/cunoc/ingenieria/inventory/view/supplyCalculatorView.java
+++ Code/ProyectoIndustrial/Proyecto-war/src/main/java/gt/udu/usac/cunoc/ingenieria/inventory/view/supplyCalculatorView.java
@@ -18,7 +18,7 @@ import javax.inject.Named;
 @Named
 @ViewScoped
 public class supplyCalculatorView implements Serializable {
-    
+
     @EJB
     private InventoryLocal inventoryLocal;
 
@@ -32,20 +32,23 @@ public class supplyCalculatorView implements Serializable {
     double costWithoutExtraCost;
     double costWithExtraCost;
     ProductionUnits selectedProduction;
-    
+
     public void searchProduction() {
         setProductionList(inventoryLocal.ProductionWithUnitsPlaces(id, productionName));
         if (getProductionList().isEmpty()) {
             MessageUtils.addSuccessMessage("Sin resultados en la busqueda");
         }
     }
-    
-    public void calculateCost() {
+
+    public void calculateCost(ProductionUnits productionUnits) {
+        selectedProduction = productionUnits;
+        System.out.println("Unidades Front: "+productionUnits.getUnits());
         if (selectedProduction == null) {
             MessageUtils.addWarningMessage("Debe seleccionar una produccion");
         } else {
             if (!inventoryLocal.getNecessarySupplies(selectedProduction).isEmpty()) {
                 setSuplyQuantity(inventoryLocal.getNecessarySupplies(selectedProduction));
+                System.out.println("Entro");
                 setCostWithoutExtraCost(inventoryLocal.costByPruductionAndQuantityWithoutExtraCost(selectedProduction));
                 setCostWithExtraCost(inventoryLocal.costByPruductionAndQuantityWithExtraCost(selectedProduction));
             } else {
@@ -53,76 +56,76 @@ public class supplyCalculatorView implements Serializable {
             }
         }
     }
-    
+
     public void cleanSearch() {
         setCostWithExtraCost(0);
         setCostWithoutExtraCost(0);
         setSuplyQuantity(null);
     }
-    
+
     public void cleanCriteria() {
         id = null;
         productionName = null;
     }
-    
+
     public void setId(Integer id) {
         this.id = id;
     }
-    
+
     public Integer getId() {
         return id;
     }
-    
+
     public void setProductionName(String productionName) {
         this.productionName = productionName;
     }
-    
+
     public String getProductionName() {
         return productionName;
     }
-    
+
     public void setSelectedProduction(ProductionUnits selectedProduction) {
         this.selectedProduction = selectedProduction;
     }
-    
+
     public void setProductionList(List<ProductionUnits> productionList) {
         this.productionList.clear();
         this.productionList = productionList;
     }
-    
+
     public List<ProductionUnits> getProductionList() {
         return productionList;
     }
-    
+
     public ProductionUnits getSelectedProduction() {
         if (selectedProduction == null) {
             selectedProduction = new ProductionUnits();
         }
         return selectedProduction;
     }
-    
+
     public List<SupplyQuantity> getSuplyQuantity() {
         return suplyQuantity;
     }
-    
+
     public void setSuplyQuantity(List<SupplyQuantity> suplyQuantity) {
         this.suplyQuantity = suplyQuantity;
     }
-    
+
     public double getCostWithoutExtraCost() {
         return costWithoutExtraCost;
     }
-    
+
     public void setCostWithoutExtraCost(double costWithoutExtraCost) {
         this.costWithoutExtraCost = costWithoutExtraCost;
     }
-    
+
     public double getCostWithExtraCost() {
         return costWithExtraCost;
     }
-    
+
     public void setCostWithExtraCost(double costWithExtraCost) {
         this.costWithExtraCost = costWithExtraCost;
     }
-    
+
 }
